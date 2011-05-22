8ed83073875741bc598891f62e6213c9778f985c
RandomCoder <randomcoder@randomcoding.co.uk>
2011-05-22 21:34:05 +0100

    Added tests for features, abv & price comparisons,

--- src/test/scala/uk/co/randomcoding/drinkfinder/model/data/DrinkDataTest.scala
+++ src/test/scala/uk/co/randomcoding/drinkfinder/model/data/DrinkDataTest.scala
@@ -5,7 +5,7 @@ package uk.co.randomcoding.drinkfinder.model.data
 
 import org.scalatest.FunSuite
 import org.scalatest.matchers.ShouldMatchers
-import uk.co.randomcoding.drinkfinder.model.drink.{Perry, Cider, Beer, Drink}
+import uk.co.randomcoding.drinkfinder.model.drink._
 
 /**
  * Tests for the filter matching capabilities of the Drink Data class/
@@ -50,10 +50,78 @@ class DrinkDataTest extends FunSuite with ShouldMatchers {
 	checkMatchedDrinks(matchingDrinks.toList, List(FirstBeer, FirstCider, FirstPerry))
   }
 
-  private def checkMatchedDrinks(matched: List[Drink], expected: List[Drink]) = {
-	val matchedSize = matched.size
+  test("Query description contains") {
+	val matcher = (drink: Drink) => drink.description.toLowerCase.contains("tasty")
+	checkMatchedDrinks(drinkData.getMatching(List(matcher)).toList, Nil)
 
-	expected should have size (matchedSize)
+	val matcherFirst = (drink: Drink) => drink.description.toLowerCase.contains("first")
+	checkMatchedDrinks(drinkData.getMatching(List(matcherFirst)).toList, List(FirstBeer, FirstCider, FirstPerry))
+  }
+
+  test("Query Abv Less Than") {
+	val abvLessThan45Matcher = (drink: Drink) => drink.abv < 4.5
+
+	checkMatchedDrinks(drinkData.getMatching(List(abvLessThan45Matcher)).toList, List(SecondBeer, SecondPerry))
+  }
+
+  test("Query ABV Equal To") {
+	val abvEqual45Matcher = (drink: Drink) => drink.abv == 4.5
+
+	checkMatchedDrinks(drinkData.getMatching(List(abvEqual45Matcher)).toList, List(FirstBeer))
+  }
+
+  test("Query ABV Greater Than") {
+	val abvGreater65Matcher = (drink: Drink) => drink.abv > 6.5
+
+	checkMatchedDrinks(drinkData.getMatching(List(abvGreater65Matcher)).toList, List(SecondCider, FirstPerry))
+  }
+
+  test("Query Price Less Than £1.30") {
+	val matcher = (drink: Drink) => drink.price < 1.30
+	checkMatchedDrinks(drinkData.getMatching(List(matcher)).toList, List(FirstPerry))
+  }
+
+  test("Query Price Equal To £1.40") {
+	val matcher = (drink: Drink) => drink.price == 1.40
+	checkMatchedDrinks(drinkData.getMatching(List(matcher)).toList, List(FirstBeer))
+  }
+
+  test("Query Price Greater Than, £1.50") {
+	val matcher = (drink: Drink) => drink.price > 1.50
+	checkMatchedDrinks(drinkData.getMatching(List(matcher)).toList, List(SecondBeer))
+  }
+
+  test("Query for Real Ales") {
+	val matcher = (drink: Drink) => drink.features.contains(RealAle)
+
+	checkMatchedDrinks(drinkData.getMatching(List(matcher)).toList, beers)
+  }
+
+  test("Query for Sweet Drinks") {
+	val matcher = (drink: Drink) => drink.features.contains(Sweet)
+	checkMatchedDrinks(drinkData.getMatching(List(matcher)).toList, List(FirstPerry))
+  }
+
+  test("Query for Dry Drinks") {
+	val matcher = (drink: Drink) => drink.features.contains(Dry)
+	checkMatchedDrinks(drinkData.getMatching(List(matcher)).toList, List(FirstCider))
+  }
+
+  test("Query for Dry Ciders") {
+	val dryMatcher = (drink: Drink) => drink.features.contains(Dry)
+	val ciderMatcher = (drink: Drink) => drink.isInstanceOf[Cider]
+
+	checkMatchedDrinks(drinkData.getMatching(List(dryMatcher, ciderMatcher)).toList, List(FirstCider))
+  }
+
+  test("Query for Dry Ciders with ABV Less Than 6.0") {
+	pending
+  }
+
+  private def checkMatchedDrinks(matched: List[Drink], expected: List[Drink]) {
+	val expectedSize = expected.size
+
+	matched should have size (expectedSize)
 
 	val expectedDrinksContainedInMatched = for {
 	  drink <- expected
