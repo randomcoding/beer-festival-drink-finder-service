0b513c31e872a531f064541f2cde3b418270cc14
RandomCoder <randomcoder@randomcoding.co.uk>
2011-06-01 08:25:11 +0100

    Removed commented code and added TODO for bind()

--- src/main/scala/uk/co/randomcoding/drinkfinder/snippet/DisplayResults.scala
+++ src/main/scala/uk/co/randomcoding/drinkfinder/snippet/DisplayResults.scala
@@ -35,22 +35,11 @@ class DisplayResults {
 	val ciders = matchingDrinks.filter(_.isInstanceOf[Cider]).toList
 	val perries = matchingDrinks.filter(_.isInstanceOf[Perry]).toList
 
-	/*SHtml.radio(List("Beers", "Ciders", "Perries"), Box("Beers"), displayDrinks)
-
-	def displayDrinks: (String) => Any = (drinkType: String) => {
-	  val drinks = drinkType match {
-		case "Beers" => beers
-		case "Ciders" => ciders
-		case "Perries" => perries
-		case _ => Nil
-	  }
-
-		"* *" #> drinkType + "(" + drinks.size + ")"
-	}*/
-
-	val results = "#beer-tab" #> "%d Beers".format(beers.size) & "#cider-tab" #> "%d Ciders".format(ciders.size) &
+	// TODO: Use the bind mechanism to put content into each results div
+	
+	/*val results = "#beer-tab" #> "%d Beers".format(beers.size) & "#cider-tab" #> "%d Ciders".format(ciders.size) &
 	  "#perry-tab" #> "%d Perries".format(perries.size)
 
-	results
+	results*/
   }
 }
