e316bd848e487c04ab211ff748084f6b743c5799
RandomCoder <randomcoder@randomcoding.co.uk>
2011-06-24 08:05:56 +0100

    re #6: MOdified display of returned drinks to be in a table format.

--- src/main/scala/uk/co/randomcoding/drinkfinder/snippet/DisplayResults.scala
+++ src/main/scala/uk/co/randomcoding/drinkfinder/snippet/DisplayResults.scala
@@ -3,14 +3,13 @@
  */
 package uk.co.randomcoding.drinkfinder.snippet
 
-import uk.co.randomcoding.drinkfinder.model.matcher.BrewerNameMatcher
-import uk.co.randomcoding.drinkfinder.model.matcher.id.BREWER_NAME
+import uk.co.randomcoding.drinkfinder.model.matcher.id.{BREWER_NAME, DRINK_NAME}
 import net.liftweb.http._
 import net.liftweb.util.Helpers._
-import scala.xml.{NodeSeq, Text}
+import scala.xml.{ NodeSeq, Text }
 import uk.co.randomcoding.drinkfinder.model.data.wbf.WbfDrinkDataAccess
-import uk.co.randomcoding.drinkfinder.model.drink.{Beer, Cider, Drink, Perry}
-import uk.co.randomcoding.drinkfinder.model.matcher.MatcherFactory
+import uk.co.randomcoding.drinkfinder.model.drink.{ Beer, Cider, Drink, Perry }
+import uk.co.randomcoding.drinkfinder.model.matcher.{MatcherFactory, BrewerNameMatcher}
 import net.liftweb.common.Logger
 
 /**
@@ -19,37 +18,62 @@ import net.liftweb.common.Logger
  */
 class DisplayResults extends Logger {
 
-	private val drinkData = new WbfDrinkDataAccess()
-	private val BrewerNameQueryRegex = "$%s=(\\.*)".format(BREWER_NAME.id)
+  private val drinkData = new WbfDrinkDataAccess()
+  private val BrewerNameQueryRegex = "$%s=(\\.*)".format(BREWER_NAME.id)
 
-	def calculateResults(in: NodeSeq): NodeSeq = {
-		import uk.co.randomcoding.drinkfinder.model.matcher.Matcher
-		val params = S.queryString openOr "No Query String"
-		debug("Received Query String: %s".format(params))
-		
-		val matchers/*: List[Matcher]*/ = params match {
-			case "No Query String" => Nil
-			case paramString: String => MatcherFactory.generate(paramString)
-		}
-		debug("Generated %d matchers:\n%s".format(matchers.size, matchers.mkString("\n\t")))
+  def calculateResults(in : NodeSeq) : NodeSeq = {
+    import uk.co.randomcoding.drinkfinder.model.matcher.Matcher
+    val params = S.queryString openOr "No Query String"
+    debug("Received Query String: %s".format(params))
 
-		val matchingDrinks: Set[Drink] = drinkData.getMatching(matchers)
+    val matchers = params match {
+      case "No Query String" => Nil
+      case paramString : String => MatcherFactory.generate(paramString)
+    }
+    debug("Generated %d matchers:\n%s".format(matchers.size, matchers.mkString("\n\t")))
 
-		debug("There are %d matching drinks:\n%s".format(matchingDrinks.size, matchingDrinks.mkString("\n\t")))
-		val beers = matchingDrinks.filter(_.isInstanceOf[Beer]).toList
-		debug("There are %d matching beers".format(beers.size))
-		val ciders = matchingDrinks.filter(_.isInstanceOf[Cider]).toList
-		debug("There are %d matching ciders".format(ciders.size))
-		val perries = matchingDrinks.filter(_.isInstanceOf[Perry]).toList
-		debug("There are %d matching perries".format(perries.size))
+    val matchingDrinks = drinkData.getMatching(matchers)
 
-		bind("results", in,
-			"beers" -> convertResultsToDisplayForm("Beers", beers),
-			"ciders" -> convertResultsToDisplayForm("Ciders", ciders),
-			"perries" -> convertResultsToDisplayForm("Perries", perries))
-	}
+    debug("There are %d matching drinks:\n%s".format(matchingDrinks.size, matchingDrinks.mkString("\n\t")))
+    val beers = matchingDrinks.filter(_.isInstanceOf[Beer])
+    debug("There are %d matching beers".format(beers.size))
+    val ciders = matchingDrinks.filter(_.isInstanceOf[Cider])
+    debug("There are %d matching ciders".format(ciders.size))
+    val perries = matchingDrinks.filter(_.isInstanceOf[Perry])
+    debug("There are %d matching perries".format(perries.size))
 
-	private def convertResultsToDisplayForm(titleText: String = "", results: List[Drink]): NodeSeq = {
-		Text("%d %s".format(results.size, titleText))
-	}
+    bind("results", in,
+      "beers" -> convertResultsToDisplayForm("Beers", beers),
+      "ciders" -> convertResultsToDisplayForm("Ciders", ciders),
+      "perries" -> convertResultsToDisplayForm("Perries", perries))
+  }
+
+  private def convertResultsToDisplayForm(titleText : String = "", results : Iterable[Drink]) : NodeSeq = {
+    val sortedDrinks = results.toList.sortBy(_.name)
+
+    for {
+      drink <- sortedDrinks
+    } yield {
+      val drinkHref = "drink?" + DRINK_NAME + "=" + drink.name
+      val abvString = "%.1f".format(drink.abv)
+      val priceString = "%.2f".format(drink.price)
+
+      <div>
+        <span class="drinkText">
+          <table class="drinkList" style="border: thin;">
+            <tr>
+              <td colspan="2" class="drinkTitle"><a href={ drinkHref }>{ drink.name }</a></td>
+            </tr>
+            <tr class="drinkData">
+              <td>ABV:{ abvString }%</td>
+              <td>£{ priceString }</td>
+            </tr>
+            <tr class="drinkDescription">
+              <td colspan="2">{ drink.description }</td>
+            </tr>
+          </table>
+        </span>
+      </div>
+    }
+  }
 }
