37cab3537a66e68e880989cb8a4180bd1f0d73a9
RandomCoder <randomcoder@randomcoding.co.uk>
2011-08-11 12:00:25 +0100

    re #48: Initial changes to add wishlist capability.
    Currently does not work, as need to re-design to enable proper use of
    cookies.

--- DrinkServiceLift/src/main/scala/uk/co/randomcoding/drinkfinder/snippet/DisplayBrewer.scala
+++ DrinkServiceLift/src/main/scala/uk/co/randomcoding/drinkfinder/snippet/DisplayBrewer.scala
@@ -10,8 +10,10 @@ import net.liftweb.util.Helpers._
 import uk.co.randomcoding.drinkfinder.model.matcher.id._
 import uk.co.randomcoding.drinkfinder.model.matcher.{ DrinkNameMatcher, BrewerNameMatcher, MatcherFactory }
 
-class DisplayBrewer extends Logger {
+class DisplayBrewer extends StatefulSnippet with Logger {
 
+	def dispatch = { case "showBrewer" => showBrewer }
+	
 	def showBrewer = {
 
 		val festivalName = urlDecode(S.param(FESTIVAL_NAME.toString).openOr("Worcester Beer, Cider and Perry Festival"))
--- DrinkServiceLift/src/main/scala/uk/co/randomcoding/drinkfinder/snippet/DisplayResults.scala
+++ DrinkServiceLift/src/main/scala/uk/co/randomcoding/drinkfinder/snippet/DisplayResults.scala
@@ -18,61 +18,63 @@ import uk.co.randomcoding.drinkfinder.model.matcher.AlwaysTrueDrinkMatcher
  * @author RandomCoder
  *
  */
-class DisplayResults extends Logger {
+class DisplayResults extends StatefulSnippet with Logger {
 
-  private val beerResultsId = "beerResults"
-  private val ciderResultsId = "ciderResults"
-  private val perryResultsId = "perryResults"
+	def dispatch = { case "calculateResults" => calculateResults }
 
-  def calculateResults = {
-    import uk.co.randomcoding.drinkfinder.model.matcher.Matcher
+	private val beerResultsId = "beerResults"
+	private val ciderResultsId = "ciderResults"
+	private val perryResultsId = "perryResults"
 
-    val params = S.queryString openOr "No Query String"
-    val festivalName = urlDecode(S.param(FESTIVAL_NAME.toString).openOr( "Worcester Beer, Cider and Perry Festival"))
+	def calculateResults = {
+		import uk.co.randomcoding.drinkfinder.model.matcher.Matcher
 
-    val festivalData = FestivalData(festivalName)
-    debug("Received Query String: %s".format(params))
+		val params = S.queryString openOr "No Query String"
+		val festivalName = urlDecode(S.param(FESTIVAL_NAME.toString).openOr("Worcester Beer, Cider and Perry Festival"))
 
-    val matchers = params match {
-      case "No Query String" => List(AlwaysTrueDrinkMatcher)
-      case paramString : String if paramString.trim.isEmpty => List(AlwaysTrueDrinkMatcher)
-      case paramString : String => MatcherFactory.generate(urlDecode(paramString))
-    }
-    debug("Generated %d matchers:\n%s".format(matchers.size, matchers.mkString("\n\t")))
+		val festivalData = FestivalData(festivalName)
+		debug("Received Query String: %s".format(params))
 
-    val matchingDrinks = festivalData.getMatching(matchers)
+		val matchers = params match {
+			case "No Query String" => List(AlwaysTrueDrinkMatcher)
+			case paramString : String if paramString.trim.isEmpty => List(AlwaysTrueDrinkMatcher)
+			case paramString : String => MatcherFactory.generate(urlDecode(paramString))
+		}
+		debug("Generated %d matchers:\n%s".format(matchers.size, matchers.mkString("\n\t")))
 
-    debug("There are %d matching drinks:\n%s".format(matchingDrinks.size, matchingDrinks.mkString("\n\t")))
-    val beers = matchingDrinks.filter(_.isInstanceOf[Beer]).toList.sortBy(_.name)
-    debug("There are %d matching beers".format(beers.size))
-    val ciders = matchingDrinks.filter(_.isInstanceOf[Cider]).toList.sortBy(_.name)
-    debug("There are %d matching ciders".format(ciders.size))
-    val perries = matchingDrinks.filter(_.isInstanceOf[Perry]).toList.sortBy(_.name)
-    debug("There are %d matching perries".format(perries.size))
+		val matchingDrinks = festivalData.getMatching(matchers)
 
-    "#resultTabs" #> generateResultTabs(beers, ciders, perries) &
-      "#beers *" #> toSummaryDisplay(beers) &
-      "#ciders *" #> toSummaryDisplay(ciders) &
-      "#perries *" #> toSummaryDisplay(perries)
-  }
+		debug("There are %d matching drinks:\n%s".format(matchingDrinks.size, matchingDrinks.mkString("\n\t")))
+		val beers = matchingDrinks.filter(_.isInstanceOf[Beer]).toList.sortBy(_.name)
+		debug("There are %d matching beers".format(beers.size))
+		val ciders = matchingDrinks.filter(_.isInstanceOf[Cider]).toList.sortBy(_.name)
+		debug("There are %d matching ciders".format(ciders.size))
+		val perries = matchingDrinks.filter(_.isInstanceOf[Perry]).toList.sortBy(_.name)
+		debug("There are %d matching perries".format(perries.size))
 
-  val anchorRef = ((anchor : String) => "#" + anchor)
+		"#resultTabs" #> generateResultTabs(beers, ciders, perries) &
+			"#beers *" #> toSummaryDisplay(beers) &
+			"#ciders *" #> toSummaryDisplay(ciders) &
+			"#perries *" #> toSummaryDisplay(perries)
+	}
 
-  private[this] def generateResultTabs(beers : Iterable[Drink], ciders : Iterable[Drink], perries : Iterable[Drink]) : NodeSeq = {
-    val beersLink = beers.toList match {
-      case Nil => Text("")
-      case _ => <li><a href={ anchorRef(beerResultsId) }>Beers ({ beers.size })</a></li>
-    }
-    val cidersLink = ciders.toList match {
-      case Nil => Text("")
-      case _ => <li><a href={ anchorRef(ciderResultsId) }>Ciders ({ ciders.size })</a></li>
-    }
-    val perryLink = perries.toList match {
-      case Nil => Text("")
-      case _ => <li><a href={ anchorRef(perryResultsId) }>Perries ({ perries.size })</a></li>
-    }
+	val anchorRef = ((anchor : String) => "#" + anchor)
 
-    <ul>{ beersLink }{ cidersLink }{ perryLink }</ul>
-  }
+	private[this] def generateResultTabs(beers : Iterable[Drink], ciders : Iterable[Drink], perries : Iterable[Drink]) : NodeSeq = {
+		val beersLink = beers.toList match {
+			case Nil => Text("")
+			case _ => <li><a href={ anchorRef(beerResultsId) }>Beers ({ beers.size })</a></li>
+		}
+		val cidersLink = ciders.toList match {
+			case Nil => Text("")
+			case _ => <li><a href={ anchorRef(ciderResultsId) }>Ciders ({ ciders.size })</a></li>
+		}
+		val perryLink = perries.toList match {
+			case Nil => Text("")
+			case _ => <li><a href={ anchorRef(perryResultsId) }>Perries ({ perries.size })</a></li>
+		}
+
+		<ul>{ beersLink }{ cidersLink }{ perryLink }</ul>
+	}
 
 }
