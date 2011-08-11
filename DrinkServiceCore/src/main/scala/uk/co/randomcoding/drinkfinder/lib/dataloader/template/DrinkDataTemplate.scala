9e94a5665767c65f7968324d118b5bff7c1050b9
RandomCoder <randomcoder@randomcoding.co.uk>
2011-08-11 08:13:50 +0100

    re #47: Removed per project gitignore files and added hidden templates!
    These should never have been ignored in the first place.

---
+++ DrinkServiceCore/src/main/scala/uk/co/randomcoding/drinkfinder/lib/dataloader/template/DrinkDataTemplate.scala
@@ -0,0 +1,103 @@
+/**
+ *
+ */
+package uk.co.randomcoding.drinkfinder.lib.dataloader.template
+
+import scala.io.Source
+import net.liftweb.common.Logger
+
+/**
+ * Representation of a template to load
+ * @author RandomCoder
+ *
+ */
+class DrinkDataTemplate(templateSource : Source) extends Logger {
+	private val LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toArray
+	
+	private val lines = templateSource.getLines.toList
+
+	private def getValueFor(key : String) : String = {
+		lines.find(_.startsWith(key)) match {
+			case Some(line) if line.matches(""".+=.+""") => line.split("=")(1).trim
+			case _ => "NOT_FOUND"
+		}
+	}
+	
+	lazy val quantityRemainingColumn = getValueFor("drink.quantity.remaining") match {
+		case "NOT_FOUND" => None
+		case value => Some(columnIndex(value))
+	}
+
+	lazy val festivalName = getValueFor("festival.name")
+
+	lazy val dataStartLine = getValueFor("data.start.line").toInt
+
+	lazy val drinkNameColumn = columnIndex(getValueFor("drink.name.column"))
+
+	lazy val drinkDescriptionColumn = getValueFor("drink.description.column") match {
+		case "NOT_FOUND" => None
+		case col => Some(columnIndex(col))
+	}
+
+	lazy val drinkPriceColumn = columnIndex(getValueFor("drink.price.column"))
+
+	lazy val drinkAbvColumn = columnIndex(getValueFor("drink.abv.column"))
+
+	lazy val brewerNameColumn = columnIndex(getValueFor("brewer.name.column"))
+
+	lazy val drinkFeatureColumn = columnIndex(getValueFor("drink.feature.column"))
+
+	lazy val drinkTypeColumn = getValueFor("drink.type.column") match {
+		case "NOT_FOUND" => -1
+		case col => columnIndex(col)
+	}
+
+	lazy val drinkType = getValueFor("drink.type") match {
+		case "NOT_FOUND" => None
+		case t => Some(t)
+	}
+
+	lazy val drinkRemainingColumn = getValueFor("drink.quantity.remaining.column") match {
+		case "NOT_FOUND" => -1
+		case col => columnIndex(col)
+	}
+
+	lazy val drinkFeatureFormat = getValueFor("drink.feature.format")
+
+	private val featureNameRegexString = """drink\.feature\.([a-zA-Z/_]+)\.column\s?=\s?([A-Z]{2})"""
+	private val featureNameRegex = featureNameRegexString.r
+	
+	/**
+	 * Get the map of drink feature names to the column that is used to identify the feature for the drink.
+	 * 
+	 *  This is only relevant if the [[#drinkFeatureFormat]] is '''SeparateColumns'''. If it is '''SingleColumn''' an empty map will be returned.
+	 */
+	lazy val drinkFeatureColumns : Map[String, Int]= {
+
+		val featureTuples = for {
+			line <- lines
+			if line.matches(featureNameRegexString)
+			val featureNameRegex(name, col) = line
+		} yield {
+			(name.replaceAll("_", " "), columnIndex(col))
+		}
+
+		featureTuples.toMap
+	}
+	
+	private def columnIndex(columnName: String) : Int = {
+		columnName.toUpperCase match {
+			case numberString if numberString.matches("""\d+""") => numberString.toInt
+			case charString if charString.matches("""[A-Z]""") => LETTERS.indexOf(charString(0))
+			case letterString if letterString.matches("""[A-Z]{2}""") => {
+				((letterIndex(letterString(0)) + 1) * 26) + letterIndex(letterString(1))
+			}
+			case _ => {
+				error("Invalid Column name %s".format(columnName))
+				-1
+			}
+		}
+	}
+	
+	private def letterIndex(letter: Char) : Int = LETTERS.indexOf(letter)
+}
