b98c451a9bb5962bb353a3a2684ed68fc5a3ae11
RandomCoder <randomcoder@randomcoding.co.uk>
2011-08-20 10:21:11 +0100

    re #53: Changed font and access to comments for add comment area.

--- DrinkServiceLift/src/main/scala/uk/co/randomcoding/drinkfinder/snippet/AddComment.scala
+++ DrinkServiceLift/src/main/scala/uk/co/randomcoding/drinkfinder/snippet/AddComment.scala
@@ -1,8 +1,7 @@
 /**
- * 
+ *
  */
 package uk.co.randomcoding.drinkfinder.snippet
-
 
 import net.liftweb.http._
 import js.JsCmd
@@ -11,10 +10,10 @@ import net.liftweb.util.Helpers._
 import uk.co.randomcoding.drinkfinder.model._
 import matcher.id.DRINK_NAME
 import comment.Comment
-import comment.DrinkComments._
+import comment.DrinkComments
 
 /**
- * TODO: Comment for 
+ * Snippet to provide logic to add a comment for a drink.
  *
  * @author RandomCoder
  *
@@ -22,25 +21,26 @@ import comment.DrinkComments._
  *
  */
 class AddComment {
-	
+
 	def render = {
 		var author = ""
 		val drinkName = urlDecode(S.param(DRINK_NAME.toString).get)
 		var commentText = ""
-			
-		def process() : JsCmd = {				
+		val comments = DrinkComments("Festiavl")
+
+		def process() : JsCmd = {
 			if (!commentText.isEmpty) {
 				if (author isEmpty) author = "Anonymous"
 				val comment = Comment(drinkName, author, commentText)
-				addComment(comment)
+				comments.addComment(comment)
 			}
-			
+
 			S.redirectTo("/drink?%s=%s".format(DRINK_NAME.toString, drinkName))
 		}
-			
+
 		"#author" #> SHtml.text(author, author = _) &
-		"#commentText" #> SHtml.textarea(commentText, commentText = _) &
-		"type=submit" #> (SHtml.onSubmitUnit(process))
+			"#commentText" #> SHtml.textarea(commentText, commentText = _) &
+			"type=submit" #> (SHtml.onSubmitUnit(process))
 	}
 
 }
