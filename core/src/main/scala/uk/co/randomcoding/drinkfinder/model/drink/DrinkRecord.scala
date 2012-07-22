26f5290f86834f580e134976924372e0f4374275
randomcoder <randomcoder@randomcoding.co.uk>
2012-07-22 08:40:58 +0100

    re #1: Added skeleton for record implementation of Drink class.

---
+++ core/src/main/scala/uk/co/randomcoding/drinkfinder/model/drink/DrinkRecord.scala
@@ -0,0 +1,36 @@
+/**
+ * Copyright (C) 2012 - RandomCoder <randomcoder@randomcoding.co.uk>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contributors:
+ *    RandomCoder <randomcoder@randomcoding.co.uk> - initial API and implementation and/or initial documentation
+ */
+package uk.co.randomcoding.drinkfinder.model.drink
+
+import net.liftweb.mongodb.record.field.ObjectIdPk
+import net.liftweb.mongodb.record.{ MongoRecord, MongoMetaRecord }
+
+/**
+ * Database record class for a Drink
+ *
+ * @author RandomCoder <randomcoder@randomcoding.co.uk>
+ *
+ * Created On: 17 Jul 2012
+ */
+class DrinkRecord private () extends MongoRecord[DrinkRecord] with ObjectIdPk[DrinkRecord] {
+  override val meta = DrinkRecord
+}
+
+object DrinkRecord extends DrinkRecord with MongoMetaRecord[DrinkRecord]
