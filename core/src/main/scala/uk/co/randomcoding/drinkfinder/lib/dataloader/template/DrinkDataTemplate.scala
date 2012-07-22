ecac93fc3c66c71df37bdfa9d18d867bdf2489f7
randomcoder <randomcoder@randomcoding.co.uk>
2012-07-22 09:00:43 +0100

    re #2: Added festival id drink class and to loader classes.
    Also updated tests to include use of festival id.

--- core/src/main/scala/uk/co/randomcoding/drinkfinder/lib/dataloader/template/DrinkDataTemplate.scala
+++ core/src/main/scala/uk/co/randomcoding/drinkfinder/lib/dataloader/template/DrinkDataTemplate.scala
@@ -11,114 +11,106 @@ import net.liftweb.common.Logger
  * @author RandomCoder
  *
  */
-class DrinkDataTemplate(templateSource : Source) extends Logger {
-	private val LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toArray
-	
-	private val lines = templateSource.getLines.toList
+class DrinkDataTemplate(templateSource: Source) extends Logger {
+  private val LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toArray
 
-	private def getValueFor(key : String) : Option[String] = {
-		lines.find(_.startsWith(key)) match {
-			case Some(line) if line.matches(""".+=.+""") => line.split("=")(1).trim match {
-				case "-1" => None
-				case value => Some(value)
-			}
-			case _ => None
-		}
-	}
-	
-	lazy val quantityRemainingColumn = getValueFor("drink.quantity.remaining") match {
-		case Some(index) => Some(columnIndex(index))
-		case _ => None 
-	}
+  private val lines = templateSource.getLines.toList
 
-	lazy val singleDrinkFeaturePrefix = getValueFor("drink.feature.prefix").getOrElse("")/* match {
-		case Some(s) : String => s + " "
-		case "NOT_FOUND" => ""
-	}*/
-	
-	lazy val festivalName = getValueFor("festival.name").get
+  private def getValueFor(key: String): Option[String] = {
+    lines.find(_.startsWith(key)) match {
+      case Some(line) if line.matches(""".+=.+""") => line.split("=")(1).trim match {
+        case "-1" => None
+        case value => Some(value)
+      }
+      case _ => None
+    }
+  }
 
-	lazy val dataStartLine = getValueFor("data.start.line").getOrElse("0").toInt
+  lazy val festivalId = getValueFor("festival.id").getOrElse("No FesticalId")
 
-	lazy val drinkNameColumn = columnIndex(getValueFor("drink.name.column").get)
+  lazy val quantityRemainingColumn = getValueFor("drink.quantity.remaining") match {
+    case Some(index) => Some(columnIndex(index))
+    case _ => None
+  }
 
-	lazy val drinkDescriptionColumn = getValueFor("drink.description.column") match {
-		case Some(col) => Some(columnIndex(col))
-		case None => None
-	}
+  lazy val singleDrinkFeaturePrefix = getValueFor("drink.feature.prefix").getOrElse("")
 
-	lazy val drinkPriceColumn = getValueFor("drink.price.column") match {
-		case Some(col) => Some(columnIndex(col))
-		case None => None
-	}
+  lazy val festivalName = getValueFor("festival.name").get
 
-	lazy val drinkAbvColumn = columnIndex(getValueFor("drink.abv.column").get)
-	/*match {
-		case Some(col) => Some(columnIndex(col))
-		case None => None
-	}*/
+  lazy val dataStartLine = getValueFor("data.start.line").getOrElse("0").toInt
 
-	lazy val brewerNameColumn = getValueFor("brewer.name.column") match {
-		case Some(col) => Some(columnIndex(col))
-		case None => None
-	}
+  lazy val drinkNameColumn = columnIndex(getValueFor("drink.name.column").get)
 
-	lazy val drinkFeatureColumn = getValueFor("drink.feature.column") match {
-		case Some(col) => Some(columnIndex(col))
-		case None => None
-	}
+  lazy val drinkDescriptionColumn = getValueFor("drink.description.column") match {
+    case Some(col) => Some(columnIndex(col))
+    case None => None
+  }
 
-	lazy val drinkTypeColumn = getValueFor("drink.type.column") match {
-		case None => -1
-		case Some(col) => columnIndex(col)
-	}
+  lazy val drinkPriceColumn = getValueFor("drink.price.column") match {
+    case Some(col) => Some(columnIndex(col))
+    case None => None
+  }
 
-	lazy val drinkType = getValueFor("drink.type") /*match {
-		case "NOT_FOUND" => None
-		case t => Some(t)
-	}*/
+  lazy val drinkAbvColumn = columnIndex(getValueFor("drink.abv.column").get)
 
-	lazy val drinkRemainingColumn = getValueFor("drink.quantity.remaining.column") match {
-		case None => -1
-		case Some(col) => columnIndex(col)
-	}
+  lazy val brewerNameColumn = getValueFor("brewer.name.column") match {
+    case Some(col) => Some(columnIndex(col))
+    case None => None
+  }
 
-	lazy val drinkFeatureFormat = getValueFor("drink.feature.format")
+  lazy val drinkFeatureColumn = getValueFor("drink.feature.column") match {
+    case Some(col) => Some(columnIndex(col))
+    case None => None
+  }
 
-	private val featureNameRegexString = """drink\.feature\.([a-zA-Z/_]+)\.column\s?=\s?([A-Z]{2})"""
-	private val featureNameRegex = featureNameRegexString.r
-	
-	/**
-	 * Get the map of drink feature names to the column that is used to identify the feature for the drink.
-	 * 
-	 *  This is only relevant if the [[#drinkFeatureFormat]] is '''SeparateColumns'''. If it is '''SingleColumn''' an empty map will be returned.
-	 */
-	lazy val drinkFeatureColumns : Map[String, Int]= {
+  lazy val drinkTypeColumn = getValueFor("drink.type.column") match {
+    case None => -1
+    case Some(col) => columnIndex(col)
+  }
 
-		val featureTuples = for {
-			line <- lines
-			if line.matches(featureNameRegexString)
-			val featureNameRegex(name, col) = line
-		} yield {
-			(name.replaceAll("_", " "), columnIndex(col))
-		}
+  lazy val drinkType = getValueFor("drink.type")
 
-		featureTuples.toMap
-	}
-	
-	private def columnIndex(columnName: String) : Int = {
-		columnName.toUpperCase match {
-			case numberString if numberString.matches("""-?\d+""") => numberString.toInt
-			case charString if charString.matches("""[A-Z]""") => LETTERS.indexOf(charString(0))
-			case letterString if letterString.matches("""[A-Z]{2}""") => {
-				((letterIndex(letterString(0)) + 1) * 26) + letterIndex(letterString(1))
-			}
-			case _ => {
-				error("Invalid Column name %s".format(columnName))
-				-1
-			}
-		}
-	}
-	
-	private def letterIndex(letter: Char) : Int = LETTERS.indexOf(letter)
+  lazy val drinkRemainingColumn = getValueFor("drink.quantity.remaining.column") match {
+    case None => -1
+    case Some(col) => columnIndex(col)
+  }
+
+  lazy val drinkFeatureFormat = getValueFor("drink.feature.format")
+
+  private val featureNameRegexString = """drink\.feature\.([a-zA-Z/_]+)\.column\s?=\s?([A-Z]{2})"""
+  private val featureNameRegex = featureNameRegexString.r
+
+  /**
+   * Get the map of drink feature names to the column that is used to identify the feature for the drink.
+   *
+   *  This is only relevant if the [[#drinkFeatureFormat]] is '''SeparateColumns'''. If it is '''SingleColumn''' an empty map will be returned.
+   */
+  lazy val drinkFeatureColumns: Map[String, Int] = {
+
+    val featureTuples = for {
+      line <- lines
+      if line.matches(featureNameRegexString)
+      val featureNameRegex(name, col) = line
+    } yield {
+      (name.replaceAll("_", " "), columnIndex(col))
+    }
+
+    featureTuples.toMap
+  }
+
+  private def columnIndex(columnName: String): Int = {
+    columnName.toUpperCase match {
+      case numberString if numberString.matches("""-?\d+""") => numberString.toInt
+      case charString if charString.matches("""[A-Z]""") => LETTERS.indexOf(charString(0))
+      case letterString if letterString.matches("""[A-Z]{2}""") => {
+        ((letterIndex(letterString(0)) + 1) * 26) + letterIndex(letterString(1))
+      }
+      case _ => {
+        error("Invalid Column name %s".format(columnName))
+        -1
+      }
+    }
+  }
+
+  private def letterIndex(letter: Char): Int = LETTERS.indexOf(letter)
 }
--- core/src/main/scala/uk/co/randomcoding/drinkfinder/model/drink/Drink.scala
+++ core/src/main/scala/uk/co/randomcoding/drinkfinder/model/drink/Drink.scala
@@ -1,30 +1,30 @@
 package uk.co.randomcoding.drinkfinder.model.drink
 
-import uk.co.randomcoding.drinkfinder.model.brewer.{Brewer, NoBrewer}
+import uk.co.randomcoding.drinkfinder.model.brewer.{ Brewer, NoBrewer }
 
-sealed case class Drink(name: String, description: String, abv: Double, price: Double) {
+sealed case class Drink(name: String, description: String, abv: Double, price: Double, festivalId: String) {
 
   private var drinkFeatures = List.empty[DrinkFeature]
-  
-  var quantityRemaining : String = "Unknown";
-  
+
+  var quantityRemaining: String = "Unknown";
+
   var brewer: Brewer = NoBrewer
 
   def addFeature(drinkFeature: DrinkFeature) {
-	drinkFeatures = drinkFeature :: drinkFeatures
+    drinkFeatures = drinkFeature :: drinkFeatures
   }
 
   def removeFeature(drinkFeature: DrinkFeature) {
-	drinkFeatures = drinkFeatures.filterNot(_ == drinkFeature)
+    drinkFeatures = drinkFeatures.filterNot(_ == drinkFeature)
   }
 
   def features = drinkFeatures
 }
 
-class Beer(name: String, description: String, abv: Double, price: Double) extends Drink(name, description, abv, price)
+class Beer(name: String, description: String, abv: Double, price: Double, festivalId: String) extends Drink(name, description, abv, price, festivalId)
 
-class Cider(name: String, description: String, abv: Double, price: Double) extends Drink(name, description, abv, price)
+class Cider(name: String, description: String, abv: Double, price: Double, festivalId: String) extends Drink(name, description, abv, price, festivalId)
 
-class Perry(name: String, description: String, abv: Double, price: Double) extends Drink(name, description, abv, price)
+class Perry(name: String, description: String, abv: Double, price: Double, festivalId: String) extends Drink(name, description, abv, price, festivalId)
 
-object NoDrink extends Drink("No Drink", "No drinks matched the search", 0, 0)
+object NoDrink extends Drink("No Drink", "No drinks matched the search", 0, 0, "No Festival")
